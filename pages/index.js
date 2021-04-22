6e30cfa70bd3adb3554a4efe8b40ec6b42f811c9
Ak-893 <60721171+Ak-893@users.noreply.github.com>
2021-04-21 20:51:19 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import {useState} from 'react'
 import CoinList from '../components/CoinList';
 import SearchBar from '../components/SearchBar';
 import Layout from '../components/Layout';
-import Datetime from './Datetime';
+import Date from './Date';
 
 
 export default function Home({filteredCoins}) {
